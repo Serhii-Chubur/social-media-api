93cc6ad0776b2e93f02beecf8469bad3dcbf2c1b
Serhii-Chubur <serhichubur15@gmail.com>
2025-02-05 19:09:40 +0200

    add image_upload func

--- social_media/models.py
+++ social_media/models.py
@@ -1,8 +1,31 @@
+import pathlib
+from typing import Any
+import uuid
+
+from django.utils.text import slugify
 from django.conf import settings
 from django.db import models
 
 
 # Create your models here.
+def image_upload(instance: Any, filename: str) -> pathlib.Path:
+
+    if isinstance(instance, Post):
+        name = f"{instance.author.username} post"
+        filename = (
+            f"{slugify(name)}"
+            f"_{uuid.uuid4()}" + pathlib.Path(filename).suffix
+        )
+        return pathlib.Path(f"posts/{instance.author.username}") / pathlib.Path(filename)
+
+    elif isinstance(instance, Profile):
+        filename = (
+            f"{slugify(instance.__str__())}"
+            f"_{uuid.uuid4()}" + pathlib.Path(filename).suffix
+        )
+        return pathlib.Path("profiles") / pathlib.Path(filename)
+
+
 class Profile(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
@@ -14,7 +37,7 @@ class Profile(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     profile_picture = models.ImageField(
-        upload_to="profile_pictures/", null=True, blank=True
+        upload_to=image_upload, null=True, blank=True
     )
     bio = models.TextField(null=True, blank=True)
 
@@ -35,7 +58,7 @@ class Post(models.Model):
     )
     post_content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    media = models.ImageField(upload_to="posts/", null=True, blank=True)
+    media = models.ImageField(upload_to=image_upload, null=True, blank=True)
     tags = models.ManyToManyField(Tag, blank=True, related_name="posts")
 
     def __str__(self):
